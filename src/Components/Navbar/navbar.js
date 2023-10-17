560012980f3b6b642f15b04999a79e0c13d08f38
Sai Rohit <sairohitp007@gmail.com>
2023-10-17 21:31:10 +0530

    “checkpoint”

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <div>
+        <div><img></img></div>
+        <div>
+          <div><Link to = ""></Link></div>
+          <div><Link to = ""></Link></div>
+          <div><Link to = ""></Link></div>
+          <div><Link to = ""></Link></div>
+        </div>
+      </div>
+      <div>
+        <div>
+          {/* iconset */}
+        </div>
+        <div>
+          {/* salutation */}
+        </div>
+      </div>
+    </div>
   )
 }
 
