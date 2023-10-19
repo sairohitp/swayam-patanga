71a7ec6bce2a6ec5beeac3ec3323a1b1402a763d
Sai Rohit <sairohitp007@gmail.com>
2023-10-19 23:54:48 +0530

    “commit”

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -1,31 +1,37 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import NavCSS from "./Navbar.module.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import NavCSS from "./Navbar.module.css";
 
 const Navbar = () => {
-  return (
-    <nav>
-      <div className='subsection'>
-      <div>
-        <div><img src = "" id = {NavCSS.navImg}/></div>
-        <div>
-          <div><Link to = ""></Link></div>
-          <div><Link to = ""></Link></div>
-          <div><Link to = ""></Link></div>
-          <div><Link to = ""></Link></div>
-        </div>
-      </div>
-      <div>
-        <div>
-          {/* iconset */}
-        </div>
-        <div>
-          {/* salutation */}
-        </div>
-      </div>
-      </div>
-    </nav>
-  )
-}
+    return (
+        <nav>
+            <div className="subsection">
+                <div>
+                    <div>
+                        <img src="" id={NavCSS.navImg} />
+                    </div>
+                    <div>
+                        <div>
+                            <Link to=""></Link>
+                        </div>
+                        <div>
+                            <Link to=""></Link>
+                        </div>
+                        <div>
+                            <Link to=""></Link>
+                        </div>
+                        <div>
+                            <Link to=""></Link>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>{/* iconset */}</div>
+                    <div>{/* salutation */}</div>
+                </div>
+            </div>
+        </nav>
+    );
+};
 
-export default Navbar
+export default Navbar;
