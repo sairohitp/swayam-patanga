492ecd3f3bbc69ecadfb64ec9aafef348d1adff3
Sai Rohit <sairohitp007@gmail.com>
2023-10-21 00:50:01 +0530

    “commit”

--- src/Components/CollectionClassifier/CollectionClassifier.js
+++ src/Components/CollectionClassifier/CollectionClassifier.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import CollectionClassifierCSS from "./CollectionClassifier.module.css"
 
 const CollectionClassifier = () => {
   return (
-    <div>CollectionClassifier</div>
+    <section id = {CollectionClassifierCSS.collectionSection}>
+        <div>
+            <div></div>
+            <div>
+                <img></img>
+            </div>
+            <div>
+                <img></img>
+            </div>
+        </div>
+    </section>
   )
 }
 
