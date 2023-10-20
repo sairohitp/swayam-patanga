f73fad787cb72dcf1b2ed930dc7acc4de9f9168a
Sai Rohit <sairohitp007@gmail.com>
2023-10-21 00:42:00 +0530

    “commit”

--- src/Components/HomeContent/HomeContent.js
+++ src/Components/HomeContent/HomeContent.js
@@ -1,32 +1,45 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import HomeContentCSS from "./HomeContent.module.css"
+import HomeContentCSS from "./HomeContent.module.css";
 
 const HomeContent = () => {
-  return (
-    <section id = {HomeContentCSS.homeSection}>
-        <div>
-            <img id = {HomeContentCSS.headerIMG}></img>
-        </div>
-        <div>
-            <p>
-            Unlocking the mesmerizing world of aviation through the lens, I am your storyteller of the skies. 
-With a passion for capturing the elegance of soaring aircraft, the thrill of aviation events, and the majesty of aerial landscapes, my portfolio is a visual journey that takes you above the clouds and into the heart of aviation's finest moments. 
-<br/><br/>
-Join me as we navigate the boundless beauty of flight, one frame at a time.
+    return (
+        <section id={HomeContentCSS.homeSection}>
+            <div>
+                <img id={HomeContentCSS.headerIMG}></img>
+            </div>
+            <div>
+                <p>
+                    Unlocking the mesmerizing world of aviation through the
+                    lens, I am your storyteller of the skies. With a passion for
+                    capturing the elegance of soaring aircraft, the thrill of
+                    aviation events, and the majesty of aerial landscapes, my
+                    portfolio is a visual journey that takes you above the
+                    clouds and into the heart of aviation's finest moments.
+                    <br />
+                    <br />
+                    Join me as we navigate the boundless beauty of flight, one
+                    frame at a time.
+                </p>
+            </div>
+            <div id = {HomeContentCSS.collectionG}>
+            <div id = {HomeContentCSS.categWindows}>
+                <div>
+                    <img></img>
+                </div>
+                <div>
+                    <img></img>
+                </div>
+            </div>
+            <div>
+                <Link to="" id={HomeContentCSS.collectionLink}>
+                    view the complete collection &rarr;
+                </Link>
+            </div>
+            </div>
+        </section>
+    );
+};
 
-            </p>
-        </div>
-        <div>
-            <div><img></img></div>
-            <div><img></img></div>
-        </div>
-        <div>
-            <Link to = "" id = {HomeContentCSS.collectionLink}>view the complete collection &rarr;</Link>
-        </div>
-    </section>
-  )
-}
-
-export default HomeContent
+export default HomeContent;
