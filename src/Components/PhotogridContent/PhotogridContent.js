10251af150b86bd3162a200fa16e86fbdd05671e
Sai Rohit <sairohitp007@gmail.com>
2023-10-21 01:37:09 +0530

    “commit”

--- src/Components/PhotogridContent/PhotogridContent.js
+++ src/Components/PhotogridContent/PhotogridContent.js
@@ -4,7 +4,27 @@ import PhotogridContentCSS from "./PhotogridContent.module.css"
 const PhotogridContent = () => {
   return (
     <section id = {PhotogridContentCSS.photogridSection}>
-        <div><img></img></div>
+        <div className={PhotogridContentCSS.tripleGridder}>
+            <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
+            <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
+            <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
+        </div>
+
+        <div className={PhotogridContentCSS.doubleGridder}>
+            <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
+            <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
+        </div>
+        
+        <div>
+            <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
+            <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
+        </div>
+
+        <div className={PhotogridContentCSS.tripleGridder}>
+            <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
+            <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
+            <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
+        </div>
     </section>
   )
 }
