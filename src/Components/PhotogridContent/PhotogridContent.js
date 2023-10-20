4fe6c9ffa613dfb5fdc42e57739b85366acfe08c
Sai Rohit <sairohitp007@gmail.com>
2023-10-21 02:18:19 +0530

    “commit”

--- src/Components/PhotogridContent/PhotogridContent.js
+++ src/Components/PhotogridContent/PhotogridContent.js
@@ -12,19 +12,41 @@ const PhotogridContent = () => {
         </div>
         
         <div className={PhotogridContentCSS.leadingDoubleGridder}>
-            <div className={[PhotogridContentCSS.IMGgridContainer, PhotogridContentCSS.doubleIMGIdentifier].join(" ")}></div>
+            <div className={[PhotogridContentCSS.IMGgridContainer, PhotogridContentCSS.doubleIMGIdentifier].join(" ")}><img></img></div>
             <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
         </div>
         
         <div className={PhotogridContentCSS.trainlingDoubleGridder}>
             <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
-            <div className={[PhotogridContentCSS.IMGgridContainer, PhotogridContentCSS.doubleIMGIdentifier].join(" ")}></div>
+            <div className={[PhotogridContentCSS.IMGgridContainer, PhotogridContentCSS.doubleIMGIdentifier].join(" ")}><img></img></div>
         </div>
 
         <div className={PhotogridContentCSS.tripleGridder}>
             <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
             <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
             <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
+        </div>
+
+        <div className={PhotogridContentCSS.leadingFripleGridder}>
+            <div className={[PhotogridContentCSS.IMGgridContainer, PhotogridContentCSS.fripleIMGIdentifier].join(" ")}><img></img></div>                
+            <div className={PhotogridContentCSS.topbottomFripleComplex}>
+                <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
+                <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
+            </div>
+        </div>
+
+        <div className={PhotogridContentCSS.tripleGridder}>
+            <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
+            <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
+            <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
+        </div>
+
+        <div className={PhotogridContentCSS.trailingFripleGridder}>
+            <div className={PhotogridContentCSS.topbottomFripleComplex}>
+                <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
+                <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
+            </div>
+            <div className={[PhotogridContentCSS.IMGgridContainer, PhotogridContentCSS.fripleIMGIdentifier].join(" ")}><img></img></div>                
         </div>
     </section>
   )
