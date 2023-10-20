66c8dec11042d5f72f5f813346f2325632cb2b81
Sai Rohit <sairohitp007@gmail.com>
2023-10-21 02:06:04 +0530

    “commit”

--- src/Components/PhotogridContent/PhotogridContent.js
+++ src/Components/PhotogridContent/PhotogridContent.js
@@ -4,21 +4,22 @@ import PhotogridContentCSS from "./PhotogridContent.module.css"
 const PhotogridContent = () => {
   return (
     <section id = {PhotogridContentCSS.photogridSection}>
+        
         <div className={PhotogridContentCSS.tripleGridder}>
             <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
             <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
             <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
         </div>
-
-        <div className={PhotogridContentCSS.doubleGridder}>
-            <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
+        
+        <div className={PhotogridContentCSS.leadingDoubleGridder}>
+            <div className={[PhotogridContentCSS.IMGgridContainer, PhotogridContentCSS.doubleIMGIdentifier].join(" ")}></div>
             <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
         </div>
         
-        {/* <div className={PhotogridContentCSS.doubleGridder}>
-            <div className={[PhotogridContentCSS.IMGgridContainer, PhotogridContentCSS.doubleIMGIdentifier].join(" ")}></div>
+        <div className={PhotogridContentCSS.trainlingDoubleGridder}>
             <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
-        </div> */}
+            <div className={[PhotogridContentCSS.IMGgridContainer, PhotogridContentCSS.doubleIMGIdentifier].join(" ")}></div>
+        </div>
 
         <div className={PhotogridContentCSS.tripleGridder}>
             <div className={PhotogridContentCSS.IMGgridContainer}><img></img></div>
