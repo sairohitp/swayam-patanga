f59c362e0282148011b1bfa13906abd21f57dabd
Sai Rohit <sairohitp007@gmail.com>
2023-10-21 20:40:59 +0530

    “commit”

--- src/Components/ContactModule/ContactModule.js
+++ src/Components/ContactModule/ContactModule.js
@@ -3,26 +3,29 @@ import ContactModuleCSS from "./ContactModule.module.css";
 
 const ContactModule = () => {
     return (
-        <section id={ContactModuleCSS.contactCSSsection}>
-            <input
-                className={ContactModuleCSS.field}
-                type="text"
-                placeholder="Name"
-            />
-            <input
-                className={ContactModuleCSS.field}
-                type="text"
-                placeholder="Email"
-            />
-            <textarea
-                className={ContactModuleCSS.field}
-                type="text"
-                placeholder="Message"
-            ></textarea>
-            <div>
-                <button id={ContactModuleCSS.submitbutton}>Submit</button>
+        <div id={ContactModuleCSS.contactCSSsection}>
+            <div id={ContactModuleCSS.contactTitle}>get in touch with me!</div>
+            <div id={ContactModuleCSS.contactCSScontent}>
+                <input
+                    className={ContactModuleCSS.field}
+                    type="text"
+                    placeholder="Name"
+                />
+                <input
+                    className={ContactModuleCSS.field}
+                    type="text"
+                    placeholder="Email"
+                />
+                <textarea
+                    className={ContactModuleCSS.field}
+                    type="text"
+                    placeholder="Message"
+                ></textarea>
+                <div>
+                    <button id={ContactModuleCSS.submitbutton}>Submit</button>
+                </div>
             </div>
-        </section>
+        </div>
     );
 };
 
