d8cf1aaca1bd1dc85b8586b23e7398db77af956c
Sai Rohit <sairohitp007@gmail.com>
2023-10-21 21:10:36 +0530

    “checkpoint”

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import "./App.css"
-import { Route, Routes } from "react-router-dom"
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Home from "./Pages/Home/Home.js"
-import Collection from "./Pages/Collection/Collection"
-import Photogrid from "./Pages/Photogrid/Photogrid"
-import Contact from "./Pages/Contact/Contact"
-import NotFound from "./Pages/NotFound/NotFound"
+import Home from "./Pages/Home/Home.js";
+import Collection from "./Pages/Collection/Collection";
+import Photogrid from "./Pages/Photogrid/Photogrid";
+import Contact from "./Pages/Contact/Contact";
+import NotFound from "./Pages/NotFound/NotFound";
 
 function App() {
-    return(
-        <Routes>
-            <Route path = "/" element = {<Home/>} />
-            <Route path = "/Collection" element = {<Collection/>} />
-            <Route path = "/Photogrid" element = {<Photogrid/>} />
-            <Route path = "/Contact" element = {<Contact/>} />
-            <Route path = "*" element= {<NotFound />} />
-        </Routes>
-    )
+    return (
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/Collection" element={<Collection />} />
+                <Route path="/Photogrid" element={<Photogrid />} />
+                <Route path="/Contact" element={<Contact />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+    );
 }
 
-export default App
+export default App;
