3986cd14b1b401bd16ffd4cbf2cbcd36bf0bd7aa
Sai Rohit <sairohitp007@gmail.com>
2023-10-22 14:52:53 +0530

    “checkpoint”

---
+++ src/Components/PhotogridContent/leadingDouble.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const leadingDouble = () => {
+  return (
+    <div>leadingDouble</div>
+  )
+}
+
+export default leadingDouble
---
+++ src/Components/PhotogridContent/trailingDouble.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const trailingDouble = () => {
+  return (
+    <div>trailingDouble</div>
+  )
+}
+
+export default trailingDouble
