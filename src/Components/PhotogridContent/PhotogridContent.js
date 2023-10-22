fa26af4cc10a8fccbb4a61778ba64df7ed343771
Sai Rohit <sairohitp007@gmail.com>
2023-10-22 14:25:26 +0530

    “checkpoint”

--- src/Components/PhotogridContent/PhotogridContent.js
+++ src/Components/PhotogridContent/PhotogridContent.js
@@ -10,73 +10,73 @@ const PhotogridContent = () => {
   return (
     <section id={PhotogridContentCSS.photogridSection}>
       <div className={PhotogridContentCSS.tripleGridder}>
-        {images.map((image, index) => (
-          <div key={index} className={PhotogridContentCSS.IMGgridContainer}>
-            <img src={image.default} alt={`Picture ${index}`} />
+        
+          <div className={PhotogridContentCSS.IMGgridContainer}>
+            <img src={""} />
           </div>
-        ))}
+        
       </div>
 
       <div className={PhotogridContentCSS.leadingDoubleGridder}>
-        {images.slice(0, 2).map((image, index) => (
-          <div key={index} className={PhotogridContentCSS.IMGgridContainer}>
-            <img src={image.default} alt={`Picture ${index}`} />
+        
+          <div className={PhotogridContentCSS.IMGgridContainer}>
+            <img src={""} />
           </div>
-        ))}
+        
       </div>
 
       <div className={PhotogridContentCSS.trainlingDoubleGridder}>
-        {images.slice(2, 4).map((image, index) => (
-          <div key={index} className={PhotogridContentCSS.IMGgridContainer}>
-            <img src={image.default} alt={`Picture ${index}`} />
+        
+          <div className={PhotogridContentCSS.IMGgridContainer}>
+            <img src={""} />
           </div>
-        ))}
+        
       </div>
 
       <div className={PhotogridContentCSS.tripleGridder}>
-        {images.slice(4, 7).map((image, index) => (
-          <div key={index} className={PhotogridContentCSS.IMGgridContainer}>
-            <img src={image.default} alt={`Picture ${index}`} />
+        
+          <div className={PhotogridContentCSS.IMGgridContainer}>
+            <img src={""} />
           </div>
-        ))}
+        
       </div>
 
       <div className={PhotogridContentCSS.leadingFripleGridder}>
-        {images.slice(7, 10).map((image, index) => (
-          <div key={index} className={PhotogridContentCSS.IMGgridContainer}>
-            <img src={image.default} alt={`Picture ${index}`} />
+        
+          <div className={PhotogridContentCSS.IMGgridContainer}>
+            <img src={""} />
           </div>
-        ))}
+        
         <div className={PhotogridContentCSS.topbottomFripleComplex}>
-          {images.slice(10, 12).map((image, index) => (
-            <div key={index} className={PhotogridContentCSS.IMGgridContainer}>
-              <img src={image.default} alt={`Picture ${index}`} />
+          
+            <div className={PhotogridContentCSS.IMGgridContainer}>
+              <img src={""} />
             </div>
-          ))}
+          
         </div>
       </div>
 
       <div className={PhotogridContentCSS.tripleGridder}>
-        {images.slice(12, 15).map((image, index) => (
-          <div key={index} className={PhotogridContentCSS.IMGgridContainer}>
-            <img src={image.default} alt={`Picture ${index}`} />
+        
+          <div className={PhotogridContentCSS.IMGgridContainer}>
+            <img src={""} />
           </div>
-        ))}
+        
       </div>
 
       <div className={PhotogridContentCSS.trailingFripleGridder}>
         <div className={PhotogridContentCSS.topbottomFripleComplex}>
-          {images.slice(15, 17).map((image, index) => (
-            <div key={index} className={PhotogridContentCSS.IMGgridContainer}>
-              <img src={image.default} alt={`Picture ${index}`} />
+          
+            <div className={PhotogridContentCSS.IMGgridContainer}>
+              <img src={""} />
             </div>
-          ))}
+          
         </div>
-        {images.slice(17, 20).map((image, index) => (
-          <div key={index} className={PhotogridContentCSS.IMGgridContainer}>
-            <img src={image.default} alt={`Picture ${index}`} />
+        
+          <div className={PhotogridContentCSS.IMGgridContainer}>
+            <img src={""} />
           </div>
-        ))}
+        
       </div>
     </section>
   );
