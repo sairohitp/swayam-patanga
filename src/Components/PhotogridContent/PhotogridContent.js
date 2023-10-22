4c704d48ecae1987f0a1ae85862ca7e7912865d7
Sai Rohit <sairohitp007@gmail.com>
2023-10-22 14:49:46 +0530

    “checkpoint”

--- src/Components/PhotogridContent/PhotogridContent.js
+++ src/Components/PhotogridContent/PhotogridContent.js
@@ -15,81 +15,81 @@ const PhotogridContent = () => {
                 <img src = {images[0]}/>
             </div>
             <div className={PhotogridContentCSS.IMGgridContainer}>
-                <img/>
+            <img src = {images[1]}/>
             </div>
             <div className={PhotogridContentCSS.IMGgridContainer}>
-                <img src = {images[0]}/>
+                <img src = {images[2]}/>
             </div>
     
         </div>
         
         <div className={PhotogridContentCSS.leadingDoubleGridder}>
             <div className={[PhotogridContentCSS.IMGgridContainer, PhotogridContentCSS.doubleIMGIdentifier].join(" ")}>
-                <img src = {images[0]}/>
+                <img src = {images[3]}/>
             </div>
             <div className={PhotogridContentCSS.IMGgridContainer}>
-                <img src = {images[0]}/>
+                <img src = {images[4]}/>
             </div>
         </div>
         
         <div className={PhotogridContentCSS.trainlingDoubleGridder}>
             <div className={PhotogridContentCSS.IMGgridContainer}>
-                <img src = {images[0]}/>
+                <img src = {images[5]}/>
             </div>
             <div className={[PhotogridContentCSS.IMGgridContainer, PhotogridContentCSS.doubleIMGIdentifier].join(" ")}>
-                <img src = {images[0]}/>
+                <img src = {images[6]}/>
             </div>
         </div>
 
         <div className={PhotogridContentCSS.tripleGridder}>
             <div className={PhotogridContentCSS.IMGgridContainer}>
-                <img src = {images[0]}/>
+                <img src = {images[7]}/>
             </div>
             <div className={PhotogridContentCSS.IMGgridContainer}>
-                <img src = {images[0]}/>
+                <img src = {images[8]}/>
             </div>
             <div className={PhotogridContentCSS.IMGgridContainer}>
-                <img src = {images[0]}/>
+                <img src = {images[9]}/>
             </div>
         </div>
 
         <div className={PhotogridContentCSS.leadingFripleGridder}>
             <div className={[PhotogridContentCSS.IMGgridContainer, PhotogridContentCSS.fripleIMGIdentifier].join(" ")}>
-                <img src = {images[0]}/>
+                <img src = {images[10]}/>
             </div>                
             <div className={PhotogridContentCSS.topbottomFripleComplex}>
                 <div className={PhotogridContentCSS.IMGgridContainer}>
-                    <img src = {images[0]}/>
+                    <img src = {images[11]}/>
                 </div>
                 <div className={PhotogridContentCSS.IMGgridContainer}>
-                    <img src = {images[0]}/>
+                    <img src = {images[12]}/>
                 </div>
             </div>
         </div>
 
         <div className={PhotogridContentCSS.tripleGridder}>
             <div className={PhotogridContentCSS.IMGgridContainer}>
-                <img src = {images[0]}/>
+                <img src = {images[13]}/>
             </div>
             <div className={PhotogridContentCSS.IMGgridContainer}>
-                <img src = {images[0]}/>
+                <img src = {images[14]}/>
             </div>
             <div className={PhotogridContentCSS.IMGgridContainer}>
-                <img src = {images[0]}/>
+                <img src = {images[15]}/>
             </div>
         </div>
 
         <div className={PhotogridContentCSS.trailingFripleGridder}>
             <div className={PhotogridContentCSS.topbottomFripleComplex}>
                 <div className={PhotogridContentCSS.IMGgridContainer}>
-                    <img src = {images[0]}/>
+                    <img src = {images[16]}/>
                 </div>
                 <div className={PhotogridContentCSS.IMGgridContainer}>
-                    <img src = {images[0]}/>
+                    <img src = {images[17]}/>
                 </div>
             </div>
             <div className={[PhotogridContentCSS.IMGgridContainer, PhotogridContentCSS.fripleIMGIdentifier].join(" ")}>
-                <img src = {images[2]}/>
+                <img src = {images[18]}/>
             </div>                
         </div>
         
