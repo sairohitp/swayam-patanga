4a013f7ae3619dc3649f93a3582d5c713a1563dd
Sai Rohit <sairohitp007@gmail.com>
2023-10-24 01:45:45 +0530

    “checkpoint”

--- src/Components/PhotogridContent/PhotogridContent.js
+++ src/Components/PhotogridContent/PhotogridContent.js
@@ -3,8 +3,9 @@ import PhotogridContentCSS from "./PhotogridContent.module.css";
 
 import TripleGrid from "./TripleGrid";
 import LeadingDouble from "./LeadingDouble";
-import TrailingDouble from "./trailingDouble";
-import LeadingFriple from "./leadingFriple";
+import TrailingDouble from "./TrailingDouble";
+import LeadingFriple from "./LeadingFriple";
+import TrailingFriple from "./TrailingFriple";
 
 const PhotogridContent = () => {
     const images = [];
@@ -18,57 +19,62 @@ const PhotogridContent = () => {
         <section id={PhotogridContentCSS.photogridSection}>
             <TripleGrid
                 imgSRC1={images[0]}
-                imgSRC2={images[0]}
-                imgSRC3={images[0]}
+                imgSRC2={images[1]}
+                imgSRC3={images[2]}
             />
 
             <LeadingDouble 
-                imgSRC1={images[0]} 
-                imgSRC2={images[0]} 
+                imgSRC1={images[3]} 
+                imgSRC2={images[4]} 
             />
 
             <TrailingDouble
-                imgSRC1={images[0]} 
-                imgSRC2={images[0]} 
+                imgSRC1={images[5]} 
+                imgSRC2={images[6]} 
             />
 
             <TripleGrid
-                imgSRC1={images[0]}
-                imgSRC2={images[0]}
-                imgSRC3={images[0]}
+                imgSRC1={images[7]}
+                imgSRC2={images[8]}
+                imgSRC3={images[9]}
             />
             
             <LeadingFriple
-                imgSRC1={images[0]}
-                imgSRC2={images[0]}
-                imgSRC3={images[0]}
+                imgSRC1={images[10]}
+                imgSRC2={images[11]}
+                imgSRC3={images[12]}
             />
 
             <TripleGrid
-                imgSRC1={images[0]}
-                imgSRC2={images[0]}
-                imgSRC3={images[0]}
-            />
+                imgSRC1={images[13]}
+                imgSRC2={images[14]}
+                imgSRC3={images[15]}
+                />
             
 
-            <div className={PhotogridContentCSS.trailingFripleGridder}>
-                <div className={PhotogridContentCSS.topbottomFripleComplex}>
-                    <div className={PhotogridContentCSS.IMGgridContainer}>
-                        <img src={images[16]} />
-                    </div>
-                    <div className={PhotogridContentCSS.IMGgridContainer}>
-                        <img src={images[17]} />
-                    </div>
-                </div>
-                <div
-                    className={[
-                        PhotogridContentCSS.IMGgridContainer,
-                        PhotogridContentCSS.fripleIMGIdentifier,
-                    ].join(" ")}
-                >
-                    <img src={images[18]} />
-                </div>
-            </div>
+            <TrailingFriple
+                imgSRC1={images[16]}
+                imgSRC2={images[17]}
+                imgSRC3={images[18]}
+            />
+
+            <LeadingDouble 
+                imgSRC1={images[19]} 
+                imgSRC2={images[20]} 
+            />
+
+            <TripleGrid
+                imgSRC1={images[21]}
+                imgSRC2={images[22]}
+                imgSRC3={images[23]}
+            />
+            
+            <TrailingDouble
+                imgSRC1={images[24]}
+                imgSRC2={images[25]} 
+            />
+
+
         </section>
     );
 };
